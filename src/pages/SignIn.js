03469357ccf58fa2f3bf23dfbce9a23328ac2bca
Mircea Bozianu <mirceabozianu@gmail.com>
2021-09-02 10:20:05 +0300

    Redesigned NavBar, Added Logo and new favicon.ico

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const SignIn = () => {
-    return <h1>Sign In</h1>
+    return (
+        <article>
+            <main>
+                <form>
+                    <fieldset>
+                        <legend>Sign In</legend>
+                        <div>
+                            
+                        </div>
+                    </fieldset>
+                </form>
+            </main>
+        </article>
+    );
 }
 export default SignIn;
